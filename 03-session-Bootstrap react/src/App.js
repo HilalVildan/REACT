f7c7dd945169ec0bf25da98f12f355fb022241a1
HilalVildan <hilalsarioglu96@gmail.com>
2022-10-16 12:32:20 +0200

    nav add

--- 03-session-Bootstrap react/src/App.js
+++ 03-session-Bootstrap react/src/App.js
@@ -1,11 +1,13 @@
 import CourseCard from './components/CourseCard';
 import data from './data'
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from './components/MyNavbar';
 
 
 function App() {
   return (
     <div>
+    <MyNavbar/>
     <CourseCard dataa={data}/>
     
     </div>
