7c013326943b9e851a08918ffbd1e7322d30cfae
HilalVildan <hilalsarioglu96@gmail.com>
2022-10-18 11:47:55 +0200

    proje cözümü

---
+++ tour-places-cözüm/src/App.js
@@ -0,0 +1,18 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
